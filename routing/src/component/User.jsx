131410bd8024b2fbc8e25ce11e1061dee052df5f
Corpjack-Aniket <corpjack.aniket@gmail.com>
2025-01-25 17:22:58 +0530

    use param hook

--- routing/src/component/User.jsx
+++ routing/src/component/User.jsx
@@ -8,7 +8,7 @@ function User() {
 <h2>      <Link to='/user/1'>User 1</Link></h2>
       <h2><Link to='/user/2'>User 2</Link></h2>
       <h2><Link to='/user/3'>User 3</Link></h2>
-      <Outlet/> //! outlet is used to display child component on same page it will not go to another component.it render child component.
+      <Outlet/> //!  outlet is used to display child component on same page it will not go to another component.it render child component.
     </div>
   )
 }
--- routing/src/component/UserDetails.jsx
+++ routing/src/component/UserDetails.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function UserDetails() {
+    // const userId = useParams();
+    // const id = userId.id;
+    const {id} = useParams(); 
   return (
     <div>
-      <h2>User Details Page......</h2>
+      <h2>User Details Page -- {id}</h2>
     </div>
   )
 }
